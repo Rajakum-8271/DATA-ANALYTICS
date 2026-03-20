5f8293f70714067f70aa866c1b2a45a860b69727
Rajakum-8271 <forever8271@gmail.com>
2026-03-20 13:34:37 +0530

    Add files via upload

---
+++ collagedata.py
@@ -0,0 +1,5 @@
+# create a chart with  your old collage data where you show your marks and year
+
+# create a dta frame with your employee working in a resturant.minimum employees are 20-25
+
+# create a chart with your file with using of pandas numpy and matplot including pie chart bar chat  and scatter plot.
